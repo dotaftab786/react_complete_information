42fd0ec07deb00a54fb3eef4f94ce6fdc312fe8b
Aftab Alam <alam69745@gmail.com>
2023-05-03 15:29:07 +0530

    First Commit

---
+++ react-redux/src/App.js
@@ -0,0 +1,8 @@
+const App = ()=>{
+  const design = (
+    <h1>Hello world</h1>
+  );
+  return design;
+}
+
+export default App;
---
+++ react_basic/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+  const App = () => {
+    const design = (
+      <h1>Starting React Practices</h1>
+    );
+    return design;
+  };
+
+export default App;
